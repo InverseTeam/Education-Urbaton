4b89e748e8477775e4958cc0f33b7ef7d7637c40
RambleTOT <90986878+RambleTOT@users.noreply.github.com>
2023-11-20 19:26:16 +0500

    create UI login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "ramble.sokol.inverseeducation"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ramble.sokol.inverseeducation"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
